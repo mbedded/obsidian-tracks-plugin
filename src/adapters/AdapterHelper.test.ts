a857fcb4dd4cee77334d17914220eb90339d5dbe
mbedded <github@mbuege.com>
2026-07-29 15:09:48 +0200

    test: Update comment to reflect migration from Jest to Vitest

--- src/adapters/AdapterHelper.test.ts
+++ src/adapters/AdapterHelper.test.ts
@@ -4,7 +4,7 @@
 import type { RequestUrlParam, RequestUrlResponsePromise } from "obsidian";
 import { test } from "vitest";
 
-// Just added so Jest doesn't throw an error.
+// Just added so Vitest doesn't throw an error.
 test("ignore this file", () => {})
 
 export function emptyRequest(_request: RequestUrlParam | string): RequestUrlResponsePromise {
